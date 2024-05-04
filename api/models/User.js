2b59b57281e960871aa292405467b0b0707ac468
Aniede Chibundu <ndubest56@gmail.com>
2024-05-04 14:33:21 +0100

    create user data

--- api/models/User.js
+++ api/models/User.js
@@ -1,40 +1,45 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = Schema({
-  username: {
-    type: String,
-    required: true,
-    max: 20,
-    unique: true,
+const UserSchema = Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: ``,
+    },
+    coverPicture: {
+      type: String,
+      default: ``,
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 6,
-  },
-  profilePicture: {
-    type: String,
-    default: ``,
-  },
-  coverPicture: {
-    type: String,
-    default: ``,
-  },
-  followers: {
-    type: Array,
-    default: [],
-  },
-  following: {
-    type: Array,
-    default: [],
-  },
-});
+  { timestamps: true }
+);
 
-const UserModel = model(`User`, UserSchema);
-
-module.exports = UserModel;
+module.exports = model(`User`, UserSchema);
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,19 @@
+const User = require("../models/User");
+
 const router = require(`express`).Router();
 
 router.get(`/`, (req, res) => {
   res.send(`Helo authentic`);
+});
+
+router.get(`/register`, async (req, res) => {
+  const user = await new User({
+    username: `lifeness`,
+    email: `life@gmail.com`,
+    password: `11111234Aa`,
+  });
+  await user.save();
+  res.send(`User created`);
 });
 
 module.exports = router;
