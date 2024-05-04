29f102d41191eb0e31a37782984143af54025c55
Aniede Chibundu <ndubest56@gmail.com>
2024-05-04 12:41:12 +0100

    middleware

--- api/index.js
+++ api/index.js
@@ -4,8 +4,22 @@ const app = express();
 const mongoose = require(`mongoose`);
 const dotenv = require(`dotenv`);
 const helmet = require(`helmet`);
+
 const morgan = require(`morgan`);
 
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log(`MongoDB connected well`));
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan(`common`));
+
+app.get(`/`, (req, res) => {
+  res.send(`Welcome to home page`);
+});
 
 app.listen(8800, () => console.log(`App is running at port 8800`));
