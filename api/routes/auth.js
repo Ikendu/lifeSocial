55f52a76aabc096e3ce6414ee526f6d9c039940f
Aniede Chibundu <ndubest56@gmail.com>
2024-05-05 08:10:26 +0100

    create user data from postman

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -6,14 +6,27 @@ router.get(`/`, (req, res) => {
   res.send(`Helo authentic`);
 });
 
-router.get(`/register`, async (req, res) => {
-  const user = await new User({
-    username: `lifeness`,
-    email: `life@gmail.com`,
-    password: `11111234Aa`,
+router.post(`/register`, async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
   });
-  await user.save();
-  res.send(`User created`);
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
+
+//Not usable in real life, creating from backend
+//   const user = await new User({
+//     username: `lifeness`,
+//     email: `life@gmail.com`,
+//     password: `11111234Aa`,
+//   });
+//   await user.save();
+//   res.send(`User created`);
