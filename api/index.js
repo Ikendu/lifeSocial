4043d0b30a98e25396c58caf7488c055b43b8ffd
Aniede Chibundu <ndubest56@gmail.com>
2024-05-18 10:36:27 +0100

    restart app

--- api/index.js
+++ api/index.js
@@ -1,26 +1,20 @@
 const express = require(`express`);
-const app = express();
-const userRouter = require(`./routes/users`);
-const authRouter = require(`./routes/auth`);
-
 const mongoose = require(`mongoose`);
-const dotenv = require(`dotenv`);
 const helmet = require(`helmet`);
-
 const morgan = require(`morgan`);
+const dotenv = require(`dotenv`);
 
 dotenv.config();
+const app = express();
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log(`MongoDB connected well`));
+  .then(() => console.log(`Connected to database`));
 
-//middleware
+//middlewares here
 app.use(express.json());
 app.use(helmet());
 app.use(morgan(`common`));
 
-app.use(`/users`, userRouter);
-app.use(`/auth`, authRouter);
-
-app.listen(8800, () => console.log(`App is running at port 8800`));
+const port = 3000;
+app.listen(port, () => console.log(`App started at port ${port}`));
