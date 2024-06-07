80b54da6ec595872bcc18368f10802fe060e546a
Aniede Chibundu <ndubest56@gmail.com>
2024-06-07 17:45:13 +0100

    post styling

--- client/src/components/feed/post/Post.jsx
+++ client/src/components/feed/post/Post.jsx
@@ -1,10 +1,21 @@
+import { MoreVert } from "@material-ui/icons";
 import "./post.css";
+import f from "../../../assets/persons/f.jpg";
 
 function Post() {
   return (
     <div className="post">
       <div className="postWrapper">
-        <div className="postTop"></div>
+        <div className="postTop">
+          <div className="topLeft">
+            <img src={f} alt="" className="postProfileImg" />
+            <span className="postUserName">Younglife</span>
+            <span className="postDate">5 mins age</span>
+          </div>
+          <div className="topRight">
+            <MoreVert />
+          </div>
+        </div>
         <div className="postCenter"></div>
         <div className="postBottom"></div>
       </div>
