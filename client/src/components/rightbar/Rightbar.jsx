44157e2293c2eac5bfe96f4535b2986065415366
Aniede Chibundu <ndubest56@gmail.com>
2024-06-08 10:03:25 +0100

    right bar sections

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,6 @@
 import gift from "../../assets/gift.png";
 import a3 from "../../assets/posts/a3.jpg";
+import h from "../../assets/persons/h.jpg";
 
 import "./rightbar.css";
 
@@ -13,7 +14,21 @@ function Rightbar() {
             <b>Lola Ama</b> and <b>3 other friends</b> have birthday today
           </span>
         </div>
-        <img src={a3} alt="rightbar ad" className="rightbarAd" />
+        <img src={a3} alt="rightbar Ad" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="righbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src={h}
+                alt="online friend image"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Akuoma Aniede</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
